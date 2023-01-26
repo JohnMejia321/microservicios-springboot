33fbfe4e74e0b293ed312e8b94207bc19cd90169
JohnMejia <john.mejias@udea.edu.co>
2023-01-25 20:02:24 -0500

    se agrega clase controlador en commons library y se refactoriza clase controller en ms usuarios

--- microservicios-usuario/src/main/java/com/formacionbdi/usuarios/controller/AlumnoController.java
+++ microservicios-usuario/src/main/java/com/formacionbdi/usuarios/controller/AlumnoController.java
@@ -1,8 +1,8 @@
 package com.formacionbdi.usuarios.controller;
 
+import com.formacionbdi.commons.controllers.CommonController;
 import com.formacionbdi.usuarios.models.Alumno;
 import com.formacionbdi.usuarios.service.AlumnoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-public class AlumnoController {
+public class AlumnoController extends CommonController<Alumno,AlumnoService> {
 
-    @Autowired
-    private AlumnoService service;
 
-    @GetMapping
-    public ResponseEntity<?> listar(){
-        return ResponseEntity.ok().body(service.findAll());
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> ver(@PathVariable Long id){
-        Optional<Alumno> o = service.findById(id);
-        if (o.isEmpty()){
-           return ResponseEntity.notFound().build();}
-        return ResponseEntity.ok(o.get());
-
-    }
-
-    @PostMapping
-    public ResponseEntity<?> crear(@RequestBody Alumno alumno){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumno));
-    }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> editar(@RequestBody Alumno alumno,@PathVariable Long id){
@@ -46,11 +27,6 @@ public class AlumnoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Long id){
-       service.deleteById(id);
-       return ResponseEntity.noContent().build();
-    }
 
 
 
