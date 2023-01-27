b480f33fb2806dd99e5036ed061a3c61d13b48c2
JohnMejia <john.mejias@udea.edu.co>
2023-01-26 21:39:58 -0500

    se agrega libreria common model de alumno en ms-curso

--- microservicios-curso/src/main/java/com/formacionbdi/cursos/MicroserviciosCursoApplication.java
+++ microservicios-curso/src/main/java/com/formacionbdi/cursos/MicroserviciosCursoApplication.java
@@ -2,10 +2,12 @@ package com.formacionbdi.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EntityScan({"com.formacionbdi.commonsalumnos.models","com.formacionbdi.cursos.model"})
 public class MicroserviciosCursoApplication {
 
 	public static void main(String[] args) {
